c1e7698c46d9953c2ce662aa9aa33eec4be4791d
Dean <dean@deansantillan.com>
2019-08-06 14:06:23 +1000

    plugin(css): add css beautify plugin

--- gulp/styles.js
+++ gulp/styles.js
@@ -7,12 +7,19 @@ import nested from 'postcss-nested';
 import cssImport from 'postcss-import';
 import mixins from 'postcss-mixins';
 import rgba from 'postcss-hexrgba';
+import cssbeautify from 'gulp-cssbeautify';
 
 const styles = () => {
 	logger('Styles Sheet');
 	return src('./app/assets/css/styles.css')
 		.pipe(
 			postCss([cssImport, mixins, nested, simpleVars, rgba, autoPrefixer()])
+		)
+		.pipe(
+			cssbeautify({
+				indent: '		',
+				autosemicolon: true
+			})
 		)
 		.pipe(dest('./app/dev/assets/css/'));
 };
